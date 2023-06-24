9f5073e3d728f16c5d19bea8283c643ec99f0af5
Yoseph <ymymym4634@gmail.com>
2023-06-24 12:41:47 +0300

    hashpassword

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -15,6 +16,18 @@ const userSchema = new mongoose.Schema({
     required: true
   },
   
+});
+
+// Hash the password before saving it to the database
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(user.password, salt);
+  user.password = hash;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
