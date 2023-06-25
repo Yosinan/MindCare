930c3bb1e14182566a9693046095698e3e36d05a
Yoseph <ymymym4634@gmail.com>
2023-06-25 20:20:54 +0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,23 +9,46 @@ import ExercisesPage from './components/ExercisesPage';
 
 const App = () => {
 
-const [currentForm,setCurrentForm]=useState('login');
-const toggleform=(formName)=>{
-  setCurrentForm(formName);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoginPage, setIsLoginPage] = useState(true);
 
-}
+  function handleLogin() {
+    setIsLoggedIn(true);
+  }
+
+  function toggleLoginPage() {
+    setIsLoginPage(!isLoginPage);
+  }
 
   return (
-    <div className='App'>
-      <div>
-        {
-          currentForm==="login"?<Login onFormSwitch={toggleform} />:<Register onFormSwitch={toggleform}/>
-          // <ExercisesPage></ExercisesPage>
-        }
-      </div>
-      
+    <div className="App">
+      {isLoggedIn ? (
+        <ExercisesPage />
+      ) : isLoginPage ? (
+        <Login onLogin={handleLogin} togglePage={toggleLoginPage} />
+      ) : (
+        <Register togglePage={toggleLoginPage} />
+      )}
     </div>
-  )
+  );
+
+// const [currentForm,setCurrentForm]=useState('login');
+// const toggleform=(formName)=>{
+//   setCurrentForm(formName);
+
+// }
+
+//   return (
+//     <div className='App'>
+//       {/* <div>
+//         {
+//           currentForm==="login"?<Login onFormSwitch={toggleform} />:<Register onFormSwitch={toggleform}/>
+//           // <ExercisesPage></ExercisesPage>
+//         }
+//       </div> */}
+      
+//     </div>
+  
 }
 
 
