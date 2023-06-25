cdad8402ded8fe57583e0f0eee194ebb165d0fd5
Yoseph <ymymym4634@gmail.com>
2023-06-25 20:40:13 +0300

    updates

--- backend/routes/chatRoute.js
+++ backend/routes/chatRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const chatRoutes = require('../controllers/chatController');
 
-app.use('/api/chat', chatRoutes);
+router.get('/api/chat', chatRoutes);
 
 module.exports = router;
