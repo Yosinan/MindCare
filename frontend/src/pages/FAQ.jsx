0cd96554e12424cdf762082be6079a977ad49734
Yoseph <ymymym4634@gmail.com>
2023-06-26 20:34:20 +0300

    update

--- frontend/src/pages/FAQ.jsx
+++ frontend/src/pages/FAQ.jsx
@@ -1,91 +1,121 @@
-import React from 'react'
+import React from 'react';
+
+const faqStyles = {
+  container: {
+    maxWidth: '800px',
+    margin: '0 auto',
+    padding: '20px',
+    fontFamily: 'Arial, sans-serif',
+  },
+  title: {
+    fontSize: '24px',
+    fontWeight: 'bold',
+    marginBottom: '20px',
+  },
+  question: {
+    fontSize: '20px',
+    fontWeight: 'bold',
+    marginBottom: '10px',
+  },
+  answer: {
+    marginBottom: '20px',
+  },
+  sublist: {
+    listStyleType: 'disc',
+    marginLeft: '20px',
+    marginBottom: '10px',
+  },
+  subitem: {
+    marginBottom: '5px',
+  },
+};
 
 function FAQ() {
   return (
-    <div>
-    <h1>Frequently Asked Questions</h1>
-      <ul>
-        <li>
-          <h3>Question: How can I manage stress effectively?</h3>
-          <p>
+    <div className="faq-container">
+      <h1 className="faq-title">Frequently Asked Questions</h1>
+      <ul className="faq-list">
+        <li className="faq-item">
+          <h3 className="faq-question">Question: How can I manage stress effectively?</h3>
+          <p className="faq-answer">
             Answer: Managing stress effectively is crucial for maintaining good mental health. Here are some strategies you can try:
           </p>
-          <ul>
-            <li>
+          <ul className="faq-sublist">
+            <li className="faq-subitem">
               <strong>1. Identify stress triggers:</strong> Pay attention to situations, people, or activities that tend to increase your stress levels. Recognizing these triggers is the first step towards managing them.
             </li>
-            <li>
+            <li className="faq-subitem">
               <strong>2. Practice relaxation techniques:</strong> Engage in activities such as deep breathing exercises, meditation, or yoga to help relax your mind and body. These techniques can reduce stress and promote a sense of calmness.
             </li>
-            <li>
+            <li className="faq-subitem">
               <strong>3. Prioritize self-care:</strong> Take care of your physical and emotional well-being. Get enough sleep, eat a balanced diet, engage in regular exercise, and make time for activities you enjoy.
             </li>
-            <li>
+            <li className="faq-subitem">
               <strong>4. Seek support:</strong> Don't hesitate to reach out to friends, family, or mental health professionals for support. Talking about your stress and concerns can help alleviate them and provide different perspectives.
             </li>
-            <li>
+            <li className="faq-subitem">
               <strong>5. Manage time effectively:</strong> Plan and organize your tasks to reduce overwhelm. Break them down into smaller, manageable steps and set realistic goals. Prioritize important tasks and delegate when possible.
             </li>
           </ul>
-          <p>
+          <p className="faq-answer">
             Remember, everyone's stress management techniques may vary. It's essential to find what works best for you and incorporate it into your daily routine.
           </p>
         </li>
-        <li>
-          <h3>Question: How can I improve my sleep quality?</h3>
-          <p>
+        <li className="faq-item">
+          <h3 className="faq-question">Question: How can I improve my sleep quality?</h3>
+          <p className="faq-answer">
             Answer: Getting enough quality sleep is crucial for overall well-being. Here are some tips to improve your sleep quality:
           </p>
-          <ul>
-            <li>
+          <ul className="faq-sublist">
+            <li className="faq-subitem">
               <strong>1. Establish a bedtime routine:</strong> Develop a consistent routine before bed to signal to your body that it's time to relax and sleep. This may include activities like reading, taking a warm bath, or practicing relaxation techniques.
             </li>
-            <li>
+            <li className="faq-subitem">
               <strong>2. Create a sleep-friendly environment:</strong> Make sure your bedroom is quiet, dark, and at a comfortable temperature. Consider using blackout curtains, earplugs, or a white noise machine to minimize disturbances.
             </li>
-            <li>
+            <li className="faq-subitem">
               <strong>3. Limit exposure to screens before bed:</strong> The blue light emitted by electronic devices can interfere with your sleep cycle. Try to avoid screens (such as smartphones, tablets, and computers) for at least an hour before bedtime.
             </li>
-            <li>
+            <li className="faq-subitem">
               <strong>4. Maintain a consistent sleep schedule:</strong> Go to bed and wake up at the same time every day, even on weekends. This helps regulate your body's internal clock and promotes better sleep.
             </li>
-            <li>
+            <li className="faq-subitem">
               <strong>5. Create a relaxing bedtime environment:</strong> Engage in calming activities before bed, such as reading a book, listening to soothing music, or practicing relaxation exercises. Avoid stimulating activities or stressful situations close to bedtime.
             </li>
           </ul>
-          <p>
+          <p className="faq-answer">
             If you continue to struggle with sleep, it may be helpful to consult with a healthcare professional for further evaluation and guidance.
           </p>
         </li>
-        <li>
-          <h3>Question: How can I improve my focus and concentration?</h3>
-          <p>
+        <li className="faq-item">
+          <h3 className="faq-question">Question: How can I improve my focus and concentration?</h3>
+          <p className="faq-answer">
             Answer: Improving focus and concentration can enhance productivity and overall cognitive performance. Here are some strategies you can try:
           </p>
-          <ul>
-            <li>
+          <ul className="faq-sublist">
+            <li className="faq-subitem">
               <strong>1. Minimize distractions:</strong> Create an environment free from distractions. Find a quiet space, turn off notifications on your devices, and set dedicated time for focused work.
             </li>
-            <li>
+            <li className="faq-subitem">
               <strong>2. Break tasks into smaller chunks:</strong> Large tasks can be overwhelming. Divide them into smaller, manageable tasks and focus on one at a time. This helps maintain clarity and prevents mental fatigue.
             </li>
-            <li>
+            <li className="faq-subitem">
               <strong>3. Practice mindfulness and meditation:</strong> Mindfulness exercises and meditation can train your mind to stay present and improve concentration. Start with short sessions and gradually increase the duration over time.
             </li>
-            <li>
+            <li className="faq-subitem">
               <strong>4. Take regular breaks:</strong> Breaks are essential for maintaining focus. Engage in activities like stretching, going for a short walk, or practicing deep breathing during breaks to rejuvenate your mind.
             </li>
-            <li>
+            <li className="faq-subitem">
               <strong>5. Prioritize sleep and rest:</strong> A well-rested mind is more focused and alert. Ensure you get enough quality sleep and take regular breaks to avoid mental exhaustion.
             </li>
           </ul>
-          <p>
+          <p className="faq-answer">
             Experiment with these techniques and find what works best for you. Remember that improving focus and concentration is a skill that can be developed with practice.
           </p>
         </li>
       </ul>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default FAQ
+export default FAQ;
