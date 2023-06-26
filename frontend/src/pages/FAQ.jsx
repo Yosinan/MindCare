8d5145d3a488fee2c51b0e99d45f0462d5171dca
Yoseph <ymymym4634@gmail.com>
2023-06-26 20:45:46 +0300

    update

--- frontend/src/pages/FAQ.jsx
+++ frontend/src/pages/FAQ.jsx
@@ -16,6 +16,10 @@ const faqStyles = {
   answer: {
     marginBottom: '20px',
   },
+  guide: {
+    fontStyle: 'italic',
+    marginTop: '10px',
+  },
 };
 
 function FAQ() {
@@ -32,12 +36,18 @@ function FAQ() {
   const renderAnswer = (answer, index) => {
     const isVisible = answersVisible[index];
     return (
-      <p
-        key={`answer-${index}`}
-        style={{ ...faqStyles.answer, display: isVisible ? 'block' : 'none' }}
-      >
-        {answer}
-      </p>
+      <div key={`answer-${index}`}>
+        <p
+          style={{ ...faqStyles.answer, display: isVisible ? 'block' : 'none' }}
+        >
+          {answer.content}
+        </p>
+        {answer.guide && (
+          <p style={faqStyles.guide}>
+            <strong>Guide:</strong> {answer.guide}
+          </p>
+        )}
+      </div>
     );
   };
 
@@ -56,53 +66,83 @@ function FAQ() {
   const faqData = [
     {
       question: 'Question: How can I manage stress effectively?',
-      answer:
-        'Answer: Managing stress effectively is crucial for maintaining good mental health. Here are some strategies you can try:...',
+      answer: {
+        content:
+          'Answer: Managing stress effectively is crucial for maintaining good mental health. Here are some strategies you can try:',
+        guide: 'For more detailed guidance on stress management, you can refer to our Stress Management Guide.',
+      },
     },
     {
       question: 'Question: How can I improve my sleep quality?',
-      answer:
-        'Answer: Getting enough quality sleep is crucial for overall well-being. Here are some tips to improve your sleep quality:...',
+      answer: {
+        content:
+          'Answer: Getting enough quality sleep is crucial for overall well-being. Here are some tips to improve your sleep quality:',
+        guide: 'For more detailed guidance on improving sleep, you can refer to our Sleep Improvement Guide.',
+      },
     },
     {
       question: 'Question: How can I improve my focus and concentration?',
-      answer:
-        'Answer: Improving focus and concentration can enhance productivity and overall cognitive performance. Here are some strategies you can try:...',
+      answer: {
+        content:
+          'Answer: Improving focus and concentration can enhance productivity and overall cognitive performance. Here are some strategies you can try:',
+        guide: 'For more detailed guidance on improving focus and concentration, you can refer to our Focus and Concentration Guide.',
+      },
     },
     {
       question: 'Question: How do I practice self-care?',
-      answer:
-        'Answer: Practicing self-care is essential for your well-being. Here are some self-care tips you can incorporate into your routine:...',
+      answer: {
+        content:
+          'Answer: Practicing self-care is essential for your well-being. Here are some self-care tips you can incorporate into your routine:',
+        guide: 'For more detailed guidance on self-care practices, you can refer to our Self-Care Guide.',
+      },
     },
     {
       question: 'Question: How can I manage work-life balance?',
-      answer:
-        'Answer: Achieving a healthy work-life balance is important. Here are some strategies to help you manage work and personal life effectively:...',
+      answer: {
+        content:
+          'Answer: Achieving a healthy work-life balance is important. Here are some strategies to help you manage work and personal life effectively:',
+        guide: 'For more detailed guidance on work-life balance, you can refer to our Work-Life Balance Guide.',
+      },
     },
     {
-      question: 'Question: How do I build resilience?',
-      answer:
-        'Answer: Building resilience is crucial for navigating life challenges. Here are some ways to develop resilience:...',
+      question: 'Question: What are some techniques for stress reduction?',
+      answer: {
+        content:
+          'Answer: Reducing stress is crucial for your mental well-being. Here are some effective techniques you can try:',
+        guide: 'For more detailed guidance on stress reduction techniques, you can refer to our Stress Reduction Guide.',
+      },
+    },
+    {
+      question: 'Question: How can I build resilience?',
+      answer: {
+        content:
+          'Answer: Building resilience can help you cope with challenges. Here are some ways to enhance your resilience:',
+        guide: 'For more detailed guidance on building resilience, you can refer to our Resilience Building Guide.',
+      },
     },
     {
       question: 'Question: How can I improve my communication skills?',
-      answer:
-        'Answer: Effective communication is essential in personal and professional relationships. Here are some tips to enhance your communication skills:...',
+      answer: {
+        content:
+          'Answer: Effective communication is essential in various aspects of life. Here are some tips to enhance your communication skills:',
+        guide: 'For more detailed guidance on improving communication skills, you can refer to our Communication Skills Guide.',
+      },
     },
     {
-      question: 'Question: How do I set realistic goals?',
-      answer:
-        'Answer: Setting realistic goals is important for success and personal growth. Here are some steps to help you set achievable goals:...',
+      question: 'Question: What are some strategies for time management?',
+      answer: {
+        content:
+          'Answer: Managing your time efficiently can increase productivity. Here are some time management strategies you can implement:',
+        guide: 'For more detailed guidance on time management, you can refer to our Time Management Guide.',
+      },
     },
     {
-      question: 'Question: How can I manage my time effectively?',
-      answer:
-        'Answer: Time management skills are valuable for productivity. Here are some techniques to help you manage your time effectively:...',
-    },
-    {
-      question: 'Question: How do I cope with change?',
-      answer:
-        'Answer: Dealing with change can be challenging. Here are some strategies to help you cope with change and adapt effectively:...',
+      question: 'Question: How can I develop a positive mindset?',
+      answer: {
+        content:
+          'Answer: Cultivating a positive mindset can improve your overall well-being. Here are some ways to develop a positive mindset:',
+        guide: 'For more detailed guidance on developing a positive mindset, you can refer to our Positive Mindset Guide.',
+      },
     },
   ];
 
