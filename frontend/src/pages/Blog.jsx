5c2f42c36fd7057579f9f8e6ee89ac2a53f9239e
Yoseph <ymymym4634@gmail.com>
2023-06-27 19:05:49 +0300

    adding blog page

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+// import Topbar from './components/topbar/Topbar'
+import Home from './pages/home/Home';
+import Single from './pages/single/Single';
+import Write from './pages/write/Write';
+import Settings from './pages/settings/Settings';
+import './App.css';
+import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom'
 
-function Blog() {
+export default function Blog() {
+    
+  const user=true;
   return (
-    <div>
-      Blog
-    </div>
+    <Router>
+      {/* <Topbar /> */}
+       <Routes>
+          <Route path='/' element={<Home />}> </Route>
+          <Route  path='/posts' element={<Home />}> </Route>
+          
+          <Route path="/post/:id" element={<Single />} > </Route>
+          <Route path="/write" element={<Write />}> </Route>
+          <Route path="/settings" element={<Settings />}> </Route>
+       </Routes>
+  
+    
+    </Router>
   )
 }
-
-export default Blog
