05ce533a21b2de5994b7779d882d49a2e992cbb7
Yoseph <ymymym4634@gmail.com>
2023-06-27 19:26:13 +0300

    update

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,28 +1,25 @@
-import React from 'react'
-// import Topbar from './components/topbar/Topbar'
+import React from 'react';
 import Home from './home/Home';
 import Single from './single/Single';
 import Write from './write/Write';
 import Settings from './settings/Settings';
 import './Blog.css';
-import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 export default function Blog() {
-    
-  const user=true;
+  const user = true;
+
   return (
     <Router>
       {/* <Topbar /> */}
-       <Routes>
-          <Route path='/' element={<Home />}> </Route>
-          <Route  path='/posts' element={<Home />}> </Route>
-          
-          <Route path="/post/:id" element={<Single />} > </Route>
-          <Route path="/write" element={<Write />}> </Route>
-          <Route path="/settings" element={<Settings />}> </Route>
-       </Routes>
-  
-    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Home />} />
+
+        <Route path="/post/:id" element={<Single />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
     </Router>
-  )
+  );
 }
