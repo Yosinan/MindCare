59e2c61ab2d085e74d1b0948bd77af84acd0d6a0
Yoseph <ymymym4634@gmail.com>
2023-06-27 20:08:59 +0300

    update

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from './home/Home';
-import Topbar from './components/topbar/Topbar';
+import Topbar from '../components/topbar/Topbar';
 import Single from './single/Single';
 import Write from './write/Write';
 import Settings from './settings/Settings';
