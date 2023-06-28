4592fadc7417434ef049783bf62d5aded2e099a2
Yoseph <ymymym4634@gmail.com>
2023-06-28 06:42:09 +0300

    update

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import Home from './home/Home';
-// import Topbar from '../components/topbar/Topbar';
-import Single from './single/Single';
-import Write from './write/Write';
-import Settings from './settings/Settings';
-import './Blog.css';
-import {   Routes, Route, Link } from 'react-router-dom';
-import { Router } from 'express';
+// import React from 'react';
+// import Home from './home/Home';
+// // import Topbar from '../components/topbar/Topbar';
+// import Single from './single/Single';
+// import Write from './write/Write';
+// import Settings from './settings/Settings';
+// import './Blog.css';
+// import {   Routes, Route, Link } from 'react-router-dom';
+// import { Router } from 'express';
 
-export default function Blog() {
-  // const user = true;
+// export default function Blog() {
+//   // const user = true;
 
-  return (
+//   return (
    
    
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<Home />} />
-        <Route path="/post/:id" element={<Single />} />
-        <Route path="/write" element={<Write />} /> 
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/posts" element={<Home />} />
+//         <Route path="/post/:id" element={<Single />} />
+//         <Route path="/write" element={<Write />} /> 
+//         <Route path="/settings" element={<Settings />} />
+//       </Routes>
       
      
-  );
-}
+//   );
+// }
