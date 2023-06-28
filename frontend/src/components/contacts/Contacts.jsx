5d3ef142b4001247caa54382ccb5c1aa6cc5d6ce
Yoseph <ymymym4634@gmail.com>
2023-06-28 18:43:21 +0300

    update on contact

--- frontend/src/components/contacts/Contacts.jsx
+++ frontend/src/components/contacts/Contacts.jsx
@@ -1,8 +1,57 @@
+import { useState } from 'react'
 import './Contacts.css'
 import photo from './photo.jpg'
 
 
 export default function Contacts() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    // Create an object with the inputted information
+    const data = {
+      name: name,
+      email: email,
+      phone: phone,
+      message: message
+    };
+    
+    try {
+      const response = await fetch('/api/contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+      
+      if (response.ok) {
+        alert('Data successfully sent to the backend!');
+        // Clear the input fields
+        setName('');
+        setEmail('');
+        setPhone('');
+        setMessage('');
+      } else {
+        console.log('Failed to send data to the backend.');
+      }
+    } catch (error) {
+      console.error('Error occurred while sending data:', error);
+    }
+  };
+  
+
+  const handleClear = () => {
+    setName('');
+    setEmail('');
+    setPhone('');
+    setMessage('');
+  };
+
   return (
     <>
       <div className="contacts">
@@ -11,7 +60,7 @@ export default function Contacts() {
         <span className="contactsTitleLg">Contacts</span>
       </div>
         <img className="contactsImg" src={photo} alt="" />
-         <h2> Get in touch and let us know how we can help</h2>
+         <h2> Get In Touch & Let Us Know How We Can Help</h2>
          
     </div>
        <div className='contactsAdress'>
@@ -19,7 +68,7 @@ export default function Contacts() {
             <div className="contactInfo">
                 <h4>Address:</h4>
                 <div className='h'> Block 54,in front of Astemariwoch Lounge</div >
-                <h4>Working days:  </h4>
+                <h4>Working Days:  </h4>
                 <div className='h'>Mon-Fri</div>
                 <h4>Phones:</h4>
                 <div className='h'>+251-913402431</div>
@@ -27,6 +76,68 @@ export default function Contacts() {
                 <h4 >E-mail:</h4>
                 <div className='e'>lemma.edea@aastu.edu.et</div>
                 <div className='e'>debebe.tilaye@aastu.edu.et</div>
+            </div>
+
+            <div className="form">
+                <h4>Miscellaneous Information:</h4>
+                <div className="h">Email us with any questions or inquiries or use our contact data.</div>
+              
+               <form onSubmit={handleSubmit}>
+        <div className="form-row">
+          <div className="form-group">
+            {/* <label htmlFor="name">Name</label> */}
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder='Name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            {/* <label htmlFor="email">Email</label> */}
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder='Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            {/* <label htmlFor="phone">Phone</label> */}
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              placeholder='phone'
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          {/* <label htmlFor="message">Message</label> */}
+          <textarea
+            id="message"
+            name="message"
+            cols={50} rows={13}
+            placeholder='text here'
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          ></textarea>
+        </div>
+        <div className="buttons">
+          <button type="submit" onClick={handleSubmit}>Send</button>
+          <button type="button" onClick={handleClear}>Clear</button>
+        </div>
+      </form>
             </div>
        </div>
     </>
