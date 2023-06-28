31551ded35fc2af39bcbba31b9d9e769029aed5f
Yoseph <ymymym4634@gmail.com>
2023-06-28 21:53:14 +0300

    update

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -93,19 +93,20 @@ useEffect(() => {
   };
 
   const handleLogout = async () => {
-    localStorage.removeItem('Token');
-    // try {
-    //   const token = localStorage.getItem('Token');
-    //   // const token = getCookie('Token');
-    //   await axios.post("http://localhost:5000/api/users/logout", {
-    //     headers: {
-    //       Authorization: `Bearer ${token}`,
-    //     },
-    //   });
-    // } catch (error) {
-    //   console.error("Error logging out:", error);
-    // //   setErrorMessage("Error logging out. Please try again.");
-    // setMessage("Error logging out. Please try again.");
+
+    // localStorage.removeItem('Token')
+    try {
+      const token = localStorage.getItem('Token');
+      // const token = getCookie('Token');
+      await axios.post("http://localhost:5000/api/users/logout", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.error("Error logging out:", error);
+    //   setErrorMessage("Error logging out. Please try again.");
+    setMessage("Error logging out. Please try again.");
     }
 
   const handleEdit = async (blogId) => {
