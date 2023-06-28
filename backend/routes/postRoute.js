c664a486a84bf6e28c09a5d59d1610a207b878ca
Yoseph <ymymym4634@gmail.com>
2023-06-28 12:25:51 +0300

    update

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 const auth = require("../middlewares/auth");
 const {getPosts, getPostById, createPost, updatePost, deletePost } = require('../controllers/postController');
 
-router.get('/api/posts',auth, getPosts);
+router.get('/api/posts', getPosts);
 router.get('/api/posts/:id',auth, getPostById);
-router.post('/api/posts',auth, createPost);
+router.post('/api/posts', createPost);
 router.put('/api/posts/:id',auth, updatePost);
 router.delete('/api/posts/:id',auth, deletePost);
 
