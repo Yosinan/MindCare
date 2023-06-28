544344fb8434d2405cda875498b5a0da4aaad65b
Yoseph <ymymym4634@gmail.com>
2023-06-28 19:14:31 +0300

    update

--- backend/routes/contactRoute.js
+++ backend/routes/contactRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const {createContact} = require('../controllers/contactController');
+const {createContact, getContacts} = require('../controllers/contactController');
 
 router.post('/api/contact',createContact);
+router.get('/api/contact',getContacts);
 
 module.exports = router;
